8ac01d57d62a80a9dc781d665caf603572ea7b6b
Marc-Olivier <marc-olivier.gagnon.4@ulaval.ca>
2019-10-03 21:21:51 -0400

    Feature/alert (#4)
    feature(notification): adding alert

--- src/containers/content/ContentContainer.js
+++ src/containers/content/ContentContainer.js
@@ -4,11 +4,13 @@ import classNames from 'classnames';
 import {Content} from 'carbon-components-react';
 import {connect} from 'react-redux';
 import {getSideNavState} from '../../action/side-nav/SideNavAction';
+import ToastContent from '../../components/toast-content/ToastContent';
 
 class ContentContainer extends React.Component {
     render() {
         return (
             <Content className={classNames({'side-nav-with': this.props.isSideNavExpanded}, 'content')}>
+                <ToastContent/>
                 {this.props.children}
             </Content>
         );
