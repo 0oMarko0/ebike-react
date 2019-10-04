ecf9d969519a15563157f6cb1bacf6330973eec2
Marco <marc.olivier.gagnon.4@gmail.com>
2019-10-03 21:21:03 -0400

    feature(alert): fix container not at the right place

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PageHeader from '../../components/page-header/PageHeader';
 import {Routes} from '../../utils/Routes';
-import ToastContent from '../../components/toast-content/ToastContent';
 import InfoTile from '../../components/tile/InfoTile';
 
 export default class HomePage extends React.Component {
@@ -16,7 +15,6 @@ export default class HomePage extends React.Component {
         return (
             <div className="bx--grid bx--grid--full-width">
                 <PageHeader title="Home" breadcrumb={breadcrumb}/>
-                <ToastContent/>
                 <div className="bx--row ">
                     <div className="bx--col-md-4 bx--col-lg-4 home_row-container">
                         <InfoTile title={"Number of restaurants"} content={45}/>
