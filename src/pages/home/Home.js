d5c4d241ff649622959bda35704c3d4d30df28e5
Marc-Olivier <marc-olivier.gagnon.4@ulaval.ca>
2019-10-03 21:08:32 -0400

    feature(tile): adding info tile (#5)

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/page-header/PageHeader';
 import {Routes} from '../../utils/Routes';
+import InfoTile from '../../components/tile/InfoTile';
 
 export default class HomePage extends React.Component {
     render() {
@@ -14,6 +15,20 @@ export default class HomePage extends React.Component {
         return (
             <div className="bx--grid bx--grid--full-width">
                 <PageHeader title="Home" breadcrumb={breadcrumb}/>
+                <div className="bx--row ">
+                    <div className="bx--col-md-4 bx--col-lg-4 home_row-container">
+                        <InfoTile title={"Number of restaurants"} content={45}/>
+                    </div>
+                    <div className="bx--col-md-4 bx--col-lg-4 home_row-container">
+                        <InfoTile title={"Number of kilometers of bike path"} content={45}/>
+                    </div>
+                    <div className="bx--col-md-4 bx--col-lg-4 home_row-container">
+                        <InfoTile title={"Active User"} content={100}/>
+                    </div>
+                    <div className="bx--col-md-4 bx--col-lg-4 home_row-container">
+                        <InfoTile title={"Nb of restaurants"} content={45}/>
+                    </div>
+                </div>
             </div>
         );
     }
